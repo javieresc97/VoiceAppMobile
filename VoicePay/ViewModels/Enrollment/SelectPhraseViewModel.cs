50717402a8408b064e4314f50b58f6b5f7b0631a
javieresc97 <javieresc97@hotmail.com>
2017-11-06 22:16:08 -0500

    Added missing file

---
+++ VoicePay/ViewModels/Enrollment/SelectPhraseViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SpeakerRecognitionAPI.Models;
+using VoicePay.Services;
+using Xamarin.Forms;
+using MvvmHelpers;
+using FormsToolkit;
+
+namespace VoicePay.ViewModels.Enrollment
+{
+    public class SelectPhraseViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Phrase> Phrases { get; set; }
+
+        public SelectPhraseViewModel()
+        {
+            IsBusy = true;
+            Phrases = new ObservableRangeCollection<Phrase>();
+        }
+
+        public async Task GetPhrases()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var phrases = await VerificationService.Instance.GetPhrasesAsync();
+                if (phrases.Any())
+                {
+                    Phrases.ReplaceRange(phrases);
+                }
+                else
+                {
+                    await DisplayAlert("¡Ups!", "El servicio no está disponible ahora. Inténtalo nuevamente.", "OK");
+                }
+            }
+            catch
+            {
+                await DisplayAlert("¡Ups!", "El servicio no está disponible ahora. Inténtalo nuevamente.", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+    }
+}
